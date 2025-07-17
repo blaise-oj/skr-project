b8b53e57a81134338b957af56e2413da7a755d0a
blaise <ojwangblaise145@gmail.com>
2025-07-17 15:05:55 +0300

    initial commit

--- admin/skr/src/App.jsx
+++ admin/skr/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './context/AuthContext.jsx';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/sidebar/sidebar.jsx';
+import Sidebar from './components/sidebar/sidebar';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
