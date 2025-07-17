29074f72aee01caad97fbee72b5b31bb28479954
blaise-oj <ojwangblaise145@gmail.com>
2025-07-17 11:39:55 +0300

    Update Login.jsx

--- admin/skr/src/pages/Login/Login.jsx
+++ admin/skr/src/pages/Login/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:4000/api/auth/login", {
+      const res = await fetch("https://skr-project-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
