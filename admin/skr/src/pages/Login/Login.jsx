1674e5a9eb60f3b094429f97da9a55c0b9cac21b
blaise <ojwangblaise145@gmail.com>
2025-07-20 12:31:32 +0300

    2ND COMMIT

--- admin/skr/src/pages/Login/Login.jsx
+++ admin/skr/src/pages/Login/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("https://skr-project-backend.onrender.com/api/auth/login", {
+      const res = await fetch("https://skr-project-backend.onrender.com/api/auth/admin-login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,8 @@ import express from "express";
 import { 
   registerAdmin, 
   loginAdmin,
-  registerUser 
+  registerUser,
+  loginUser 
 } from "../controllers/auth.controller.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js"; // New middleware
 import User from "../models/user.model.js"; // Import User model for email verification
@@ -15,7 +16,9 @@ router.post("/register", registerUser); // Regular user registration
 router.get("/verify-email", verifyEmail);
 
 
-router.post("/login", loginAdmin); // Shared login endpoint
+router.post("/user-login", loginUser);   // for regular users
+router.post("/admin-login", loginAdmin); // for admins only
+
 
 // Protected admin routes
 router.post("/admin/register", verifyAdmin, registerAdmin); // Admin-only registration
