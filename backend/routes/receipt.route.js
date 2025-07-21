19f9c398855fe12a994b392bef4dd0208375138b
blaise <ojwangblaise145@gmail.com>
2025-07-21 21:52:23 +0300

    3rd commit

--- backend/routes/receipt.route.js
+++ backend/routes/receipt.route.js
@@ -3,14 +3,13 @@ import receipt from "../models/receipt.model.js"
 import {getReceipts,getReceipt,createReceipt,deleteReceipt,searchByTrackingId,markAsWithdrawn,generateReceiptPDF, generateQRCode, updateReceipt} from "../controllers/receipt.controller.js"
 import { validateReceipt } from "../middleware/validateReceipt.js"
 import { verifyToken } from "../middleware/verifyToken.js";
+import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 const router = express.Router()
 
 router.get('/',getReceipts);
 router.get("/:id",getReceipt);
-router.post("/",validateReceipt, createReceipt,);
-
-router.get('/track/:trackingId', searchByTrackingId);
+router.post("/", verifyAdmin, validateReceipt, createReceipt);
 
 
 router.patch("/:trackingId/withdraw", markAsWithdrawn);
